1bccac418d3b9e57f944cf8d53ce3d032998855b
luk <luk.fabian@gmx.de>
2024-12-15 14:30:29 +0100

    refactor(run): removed non-deadlockfuzzer related functionality

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import os
 import re
-import time
 import signal
 import optparse
 import subprocess
@@ -12,6 +11,9 @@ out_files_base = os.path.join(BASE_PATH, "outfiles")
 exec_num_file_path = os.path.join(BASE_PATH, "execNumberFile.txt")
 run_xml_file = os.path.join(BASE_PATH, "run.xml")
 stable_xml_file = os.path.join(BASE_PATH, "run_stable.xml")
+
+ant_path = pathlib.Path.joinpath(BASE_PATH, pathlib.Path("lib"), pathlib.Path("ant-contrib.jar"))
+pre_cmds = f"export ANT_HOME={ant_path} export JAVA_HOME=/home/swim/.jdks/corretto-1.8.0_432"
 
 
 def get_total_time(benchmark, analysis_name):
@@ -34,31 +36,18 @@ def get_total_time(benchmark, analysis_name):
 
 
 def run_analysis(analysis_name, bench_name, single_run_timeout, num_iter=None, total_timeout=None):
-    pre_cmds = "export ANT_HOME=/home/apache-ant-1.7.1-bin/apache-ant-1.7.1;" \
-               "export JAVA_HOME=/home/jdk1.6.0_45"
-
-    if bench_name == "eclipse":
-        workspace_path = os.path.join(BASE_PATH, "external_benchmarks", "dacapo-9.12-bach_mini", "scratch_eclipse",
-                                      "workspace")
-        if os.path.exists(workspace_path):
-            zip_path = os.path.join(BASE_PATH, "external_benchmarks", "dacapo-9.12-bach_mini", "scratch_eclipse",
-                                    "full-source-R3_0-default.zip")
-            remove_workspace = "rm -r {}".format(workspace_path)
-            renew_workspace = "unzip {} -d {}".format(zip_path, workspace_path)
-            pre_cmds = "{}; {}; {}".format(pre_cmds, remove_workspace, renew_workspace)
-
     if num_iter is not None:
         print("\nRunning {} with {} iterations.".format(analysis_name, num_iter))
         prepare_xml_file(analysis_name)
         create_out_folders(out_files_base, bench_name, analysis_name)
 
-        cmd = "{}; ant -f run.xml {}".format(pre_cmds, bench_name + "_instr")
+        cmd = "{}; ant -f run.xml {}".format(pre_cmds, bench_name)
         execute_cmd(cmd)
 
         for i in range(0, num_iter):
             print("\t iteration {}".format(i))
             out_file = os.path.join(out_files_base, bench_name, analysis_name, "log_{}.txt".format(i))
-            cmd = "{}; ant -f run.xml {} > {}".format(pre_cmds, bench_name + "_analysis", out_file)
+            cmd = "{}; ant -f run.xml {} > {}".format(pre_cmds, bench_name, out_file)
             execute_until_succeed(cmd, single_run_timeout)
     elif total_timeout is not None:
         print("\nRunning {} with {:.0f}s total analysis timeout.".format(analysis_name, total_timeout))
@@ -66,7 +55,7 @@ def run_analysis(analysis_name, bench_name, single_run_timeout, num_iter=None, t
         create_out_folders(out_files_base, bench_name, analysis_name)
         total_time = get_total_time(bench_name, analysis_name)
 
-        cmd = "{}; ant -f run.xml {}".format(pre_cmds, bench_name + "_instr")
+        cmd = "{}; ant -f run.xml {}".format(pre_cmds, bench_name)
         execute_cmd(cmd)
 
         if os.path.exists(exec_num_file_path):
@@ -75,62 +64,10 @@ def run_analysis(analysis_name, bench_name, single_run_timeout, num_iter=None, t
         while total_time < total_timeout:
             print("\titeration {}".format(i))
             out_file = os.path.join(out_files_base, bench_name, analysis_name, "log_{}.txt".format(i))
-            cmd = "{}; ant -f run.xml {} > {}".format(pre_cmds, bench_name + "_analysis", out_file)
+            cmd = "{}; ant -f run.xml {} > {}".format(pre_cmds, bench_name, out_file)
             execute_until_succeed(cmd, single_run_timeout)
             total_time = get_total_time(bench_name, analysis_name)
             i += 1
-
-
-def run_jigsaw(timeout):
-    pre_cmds = "export ANT_HOME=/home/apache-ant-1.7.1-bin/apache-ant-1.7.1;" \
-               "export JAVA_HOME=/home/jdk1.6.0_45"
-    analysis_name = "DeadlockFuzzer"
-    print("\nRunning {} with {:.0f}s total analysis timeout.".format(analysis_name, timeout))
-    prepare_xml_file(analysis_name)
-    create_out_folders(out_files_base, bench_name, analysis_name)
-    cmd = "{}; ant -f run.xml {}".format(pre_cmds, bench_name + "_instr")
-    execute_cmd(cmd)
-    out_file = os.path.join(out_files_base, bench_name, analysis_name, "log_{}.txt".format(0))
-    analysis_cmd = "{}; ant -f run.xml {} > {}".format(pre_cmds, bench_name + "_analysis", out_file)
-    harness_cmd = "python3 {}".format(os.path.join(BASE_PATH, "external_benchmarks", "jigsaw", "harness_kill.py"))
-
-    analysis_process = subprocess.Popen(analysis_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
-                                        preexec_fn=os.setsid)
-    time.sleep(5)
-    harness_process = subprocess.Popen(harness_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
-                                       preexec_fn=os.setsid)
-    time.sleep(timeout)
-    os.killpg(os.getpgid(analysis_process.pid), signal.SIGTERM)
-    os.killpg(os.getpgid(harness_process.pid), signal.SIGTERM)
-    print("experiment timed out!")
-
-    analysis_name = "SPDOnline"
-    print("\nRunning {} with {:.0f}s total analysis timeout.".format(analysis_name, timeout))
-    prepare_xml_file(analysis_name)
-    create_out_folders(out_files_base, bench_name, analysis_name)
-    cmd = "{}; ant -f run.xml {}".format(pre_cmds, bench_name + "_instr")
-    execute_cmd(cmd)
-    if os.path.exists(exec_num_file_path):
-        os.remove(exec_num_file_path)
-    remaining_timeout = timeout
-    try:
-        i = 0
-        while remaining_timeout > 0:
-            out_file = os.path.join(out_files_base, bench_name, analysis_name, "log_{}.txt".format(i))
-            analysis_cmd = "{}; ant -f run.xml {} > {}".format(pre_cmds, bench_name + "_analysis", out_file)
-            start_time = time.time()
-            analysis_process = subprocess.Popen(analysis_cmd, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
-            time.sleep(5)
-            harness_process = subprocess.Popen(harness_cmd, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
-
-            analysis_process.communicate(timeout=remaining_timeout)
-            remaining_timeout = remaining_timeout - (time.time() - start_time)
-            os.killpg(os.getpgid(harness_process.pid), signal.SIGTERM)
-            i += 1
-    except:
-        os.killpg(os.getpgid(analysis_process.pid), signal.SIGTERM)
-        os.killpg(os.getpgid(harness_process.pid), signal.SIGTERM)
-        print("experiment timed out!")
 
 
 def prepare_xml_file(analysis_name):
@@ -164,18 +101,7 @@ def execute_cmd(cmd, timeout=None):
 
 
 def run(bench_name, single_run_timeout, num_iter):
-    if bench_name == "jigsaw":
-        run_jigsaw(single_run_timeout)
-    else:
-        if bench_name in ["JDBCMySQL-1", "JDBCMySQL-2", "JDBCMySQL-3", "JDBCMySQL-4"]:
-            os.system("/etc/init.d/mysql start")
-
-        run_analysis("DeadlockFuzzer", bench_name, single_run_timeout, num_iter=num_iter)
-        deadlockfuzzer_total_time = get_total_time(bench_name, "DeadlockFuzzer")
-        run_analysis("SPDOnline", bench_name, single_run_timeout, total_timeout=deadlockfuzzer_total_time)
-        run_analysis("DeadlockFuzzer-I", bench_name, single_run_timeout, num_iter=num_iter)
-        run_analysis("SPDOnline-I", bench_name, single_run_timeout, num_iter=num_iter)
-        run_analysis("Native", bench_name, single_run_timeout, num_iter=num_iter)
+    run_analysis("DeadlockFuzzer", bench_name, single_run_timeout, num_iter=num_iter)
 
 
 def convert2std(input_filename, output_filename):
@@ -220,11 +146,11 @@ def record_trace(bench_name):
     print("\nRecording traces.")
     prepare_xml_file(analysis_name)
     create_out_folders(out_files_base, bench_name, analysis_name)
-    cmd = "{}; ant -f run.xml {}".format(pre_cmds, bench_name + "_instr")
+    cmd = "{}; ant -f run.xml {}".format(pre_cmds, bench_name)
     execute_cmd(cmd)
 
     log_out_file = os.path.join(out_files_base, bench_name, analysis_name, "log.txt")
-    cmd = "{}; ant -f run.xml {} > {}".format(pre_cmds, bench_name + "_analysis", log_out_file)
+    cmd = "{}; ant -f run.xml {} > {}".format(pre_cmds, bench_name, log_out_file)
     execute_cmd(cmd)
 
     std_out_file = os.path.join(out_files_base, bench_name, analysis_name, "{}_dlf.std".format(bench_name))
@@ -240,44 +166,7 @@ def create_out_folders(path, program_name, analysis_name):
 
 def get_benchmarks():
     return [
-        ("Deadlock", 10),
-        ("Picklock", 10),
-        ("Bensalem", 10),
-        ("Transfer", 10),
-        ("Test-Dimmunix", 10),
-        ("StringBuffer", 10),
-        ("Test-Calfuzzer", 10),
-        ("DiningPhil", 10),
-        ("HashTable", 20),
-        ("Account", 20),
-        ("Log4j2", 20),
-        ("Dbcp1", 60),
-        ("Dbcp2", 60),
-        ("RayTracer", 200),
-        ("Tsp", 200),
-        ("jigsaw", 120),
-        ("elevator", 200),
-        ("JDBCMySQL-1", 20),
-        ("JDBCMySQL-2", 20),
-        ("JDBCMySQL-3", 20),
-        ("JDBCMySQL-4", 20),
-        ("hedc", 100),
-        ("cache4j", 200),
-        ("lusearch", 120),
-        ("ArrayList", 60),
-        ("Stack", 60),
-        ("IdentityHashMap", 60),
-        ("LinkedList", 60),
-        ## ("Swing", None),
-        ("Sor", 200),
-        ("HashMap", 60),
-        ("Vector", 120),
-        ("LinkedHashMap", 60),
-        ("WeakHashMap", 60),
-        ("montecarlo", 200),
-        ("TreeMap", 60),
-        ("eclipse", 120),
-        ("TestPerf", 200),
+        ("deadlockfuzzer", 10),
     ]
 
 
